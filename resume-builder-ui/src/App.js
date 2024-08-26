ac81fb290801924743dce9432c56b8e9dbbd6679
con-007 <mayankdubey580@gmail.com>
2024-08-26 19:20:24 +0530

    Show components on UI

--- resume-builder-ui/src/App.js
+++ resume-builder-ui/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import UploadResumeForm from './components/UploadResumeForm';
+import FetchResume from './components/FetchResume';
+
 function App() {
   return (
     <div className="App">
       <h1>Resume Builder Demo</h1>
+      <br/>
+      <h2>Save Resume Details</h2>
+      <UploadResumeForm/>
+      <br/>
+      <hr/>
+      <h2>Fetch Resume Details</h2>
+      <FetchResume/>
     </div>
   );
 }
